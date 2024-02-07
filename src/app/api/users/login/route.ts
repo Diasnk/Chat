f0a8ad28d1e5ccee28cdf0ac0d0250dd85c0966c
Diasnk <diasnugumanov258@gmail.com>
2024-02-07 22:15:22 +0600

    login

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,34 @@
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import {NextRequest, NextResponse} from "next/server";
+import bcryptjs from "bcryptjs";
+
+
+connect()
+
+
+export async function POST(request: NextRequest){
+    try {
+        const reqBody = await request.json()
+        const {email, password} = reqBody
+        console.log(reqBody)
+
+
+        // check if user exists
+
+        const user = await User.findOne({email})
+        if(!user){
+            return NextResponse.json({error: "User does not exist"}, {status: 400})
+        }
+
+
+        // check if password is correct 
+
+        const validPassowrd = await bcryptjs.compare(password, user.password)
+        if(validPassowrd!){
+            return NextResponse.json({error: "Invalid password or username"}, {status: 400})
+        }
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
